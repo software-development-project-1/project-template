acda04f7ed45a20733f02b0a4a0f7c3b58cfef22
Tatiana <tatiana.lyubavskaya@gmail.com>
2024-04-12 14:07:18 +0300

    User class and UserRepositiry were added

---
+++ src/main/java/fi/haagahelia/quizzer/model/User.java
@@ -0,0 +1,88 @@
+package fi.haagahelia.quizzer.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId", nullable = false, updatable = false)
+    private Long userId;
+
+    // Username with unique constraint
+    @Column(name = "userName", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    public User() {
+
+    }
+
+    public User(String userName, String role, String firstName, String lastName) {
+        super();
+        this.userName = userName;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", userName=" + userName + ", role=" + role + ", firstName=" + firstName
+                + ", lastName=" + lastName + "]";
+    }
+
+}
