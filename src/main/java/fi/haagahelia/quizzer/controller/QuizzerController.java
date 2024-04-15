ba5014d212608cc120f5a0851717c145d10d3d7a
Janphan <janphan061@gmail.com>
2024-04-15 22:43:57 +0300

    add quizzer controller -Hong

---
+++ src/main/java/fi/haagahelia/quizzer/controller/QuizzerController.java
@@ -0,0 +1,32 @@
+package fi.haagahelia.quizzer.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+// import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
+
+import fi.haagahelia.quizzer.model.Difficulty;
+import fi.haagahelia.quizzer.model.Quizz;
+import fi.haagahelia.quizzer.repository.DifficultyRepository;
+import fi.haagahelia.quizzer.repository.QuestionRepository;
+import fi.haagahelia.quizzer.repository.QuizzRepository;
+import fi.haagahelia.quizzer.repository.StatusRepository;
+
+@Controller
+public class QuizzerController {
+
+    @Autowired
+    private QuizzRepository quizzRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private StatusRepository statusRepository;
+    @Autowired
+    private DifficultyRepository difficultyRepository;
+
+}
