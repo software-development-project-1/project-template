5b20277b51e4626ee440859792aca9f60fc9fe1c
Blazej3 <b.goszczyn88@gmail.com>
2024-04-16 15:00:36 +0300

    tasks for Story 3/ signed Blazej

--- src/main/java/fi/haagahelia/quizzer/controller/QuizzerController.java
+++ src/main/java/fi/haagahelia/quizzer/controller/QuizzerController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 // import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.ModelAttribute;
 // import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +41,20 @@ public class QuizzerController {
     public String recipientList(Model model) {
         model.addAttribute("quizzlist", quizzRepository.findAll());
         return "quizzlist";
+    }
+
+    @GetMapping(value = "/editquiz/{id}")
+    public String editQuizForm(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("quizz", quizzRepository.findById(id));
+        model.addAttribute("statuses", statusRepository.findAll());
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "editquizz.html";
+    }
+
+    @PostMapping(value = "/savequizz")
+    public String save(Quizz quizz) {
+        quizzRepository.save(quizz);
+        return "redirect:/quizzlist";
     }
 
 }
