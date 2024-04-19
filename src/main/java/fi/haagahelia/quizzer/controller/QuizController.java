31d09fcb4055ab7ce3d44168bb06f3a306e37ba1
Phat Nguyen <phattrong.a@gmail.com>
2024-04-19 10:34:38 +0300

    Add QuestionController

--- src/main/java/fi/haagahelia/quizzer/controller/QuizController.java
+++ src/main/java/fi/haagahelia/quizzer/controller/QuizController.java
@@ -32,9 +32,6 @@ public class QuizController {
 	private static final Logger logger = LoggerFactory.getLogger(QuizController.class);
 	@Autowired
 	private QuizRepository qrepository;
-	// @Autowired
-	// private CategoryRepository catrepository;
-
 	@Autowired
 	private QuestionRepository questionrepository;
 
@@ -106,72 +103,6 @@ public class QuizController {
 		qrepository.deleteById(id);
 		return "redirect:/";
 	}
-
-	@RequestMapping(value = "/addquestiontolist/{id}", method = RequestMethod.GET)
-	public String addQuestions(@PathVariable("id") Long id, Model model) {
-	
-		Optional<Quiz> quizOptional = qrepository.findById(id);
-		if (quizOptional.isPresent()) {
-			Quiz quiz = quizOptional.get();
-
-			Question newQuestion = new Question();
-			newQuestion.setQuiz(quiz);
-
-			model.addAttribute("newquestion", newQuestion);
-			model.addAttribute("quiz", quiz);
-
-			return "addQuestionToList";
-		} else {
-			System.out.println("Something went wrong");
-			return "error";
-		}
-	}
-
-	@RequestMapping(value = "/questionList/{id}", method = RequestMethod.GET)
-	public String questionList(@PathVariable("id") Long id, Model model) {
-		Optional<Quiz> quizOptional = qrepository.findById(id);
-		if (quizOptional.isPresent()) {
-			Quiz quiz = quizOptional.get();
-			List<Question> questionList = questionrepository.findByQuiz(quiz);
-
-			model.addAttribute("questionList", questionList);
-			model.addAttribute("quiz", quiz);
-
-		}
-		return "questionList";
-
-	}
-	@RequestMapping(value = "/saveQuestionToList", method = RequestMethod.POST)
-	public String saveQuestionToList(Question newQuestion) {
-		Long quizId = newQuestion.getQuiz().getId();
-		questionrepository.save(newQuestion);
-		return "redirect:/questionList/"+quizId;
-	}
-
-	// Delete question by id:
-	@RequestMapping(value = "/deleteQuestion/{id}", method = RequestMethod.GET)
-	public String deleteQuestion(@PathVariable("id") Long id, Model model) {
-
-		// To retrieve the Quiz Id to put it into the redirect URL
-		Optional<Question> questionOptional = questionrepository.findById(id);
-		Question question = questionOptional.get();
-		Quiz quiz = question.getQuiz();
-		Long quizId = quiz.getId();
-
-		questionrepository.deleteById(id);
-		return "redirect:/questionList/" + quizId;
-	}
-
-	@RequestMapping(value = "/editQuestion/{id}", method = RequestMethod.GET)
-	public String editProduct(@PathVariable("id") Long id, Model model) {
-		Optional<Question> questionOptional = questionrepository.findById(id);
-		Question question = questionOptional.get();
-		Quiz quiz = question.getQuiz();
-		model.addAttribute("quiz", quiz);
-		model.addAttribute("questionToUpdate", question);
-		return "editQuestion";
-	}
-
 	@GetMapping("/quiz/newest")
 	public String listNewestQuizzes(Model model) {
 		List<Quiz> quizzes = qrepository.findAllByOrderByCreatedAtDesc();
@@ -198,31 +129,6 @@ public class QuizController {
 		List<Quiz> quizzes = qrepository.findByPublished(false);
 		model.addAttribute("quizzes", quizzes);
 		return "quizzesList";
-	}
-
-	@RequestMapping(value = "/questionList/{id}/{difficultyLevel}", method = RequestMethod.GET)
-	public String questionListByDifficultyLevel(@PathVariable("id") Long id, @PathVariable("difficultyLevel") String difficultyLevel, Model model) {
-
-		Optional<Quiz> quizOptional = qrepository.findById(id);
-		ArrayList<Question> questionsByDifficultyLevel = new ArrayList<Question>();
-
-		if (quizOptional.isPresent()) {
-
-			Quiz quiz = quizOptional.get();
-			List<Question> questionList = questionrepository.findByQuiz(quiz);
-
-			for (Question question : questionList) {
-				if (question.getDifficultyLevel().equals(difficultyLevel)) {
-					questionsByDifficultyLevel.add(question);
-				}
-			}
-
-			model.addAttribute("questionList", questionsByDifficultyLevel);
-			model.addAttribute("quiz", quiz);
-
-		}
-		return "questionList";
-
 	}
 
 }
