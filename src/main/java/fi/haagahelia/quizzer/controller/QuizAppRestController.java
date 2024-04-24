8b65f5a05bd54aa2d0130585e18abb816be1f936
madaraDance <maksim.minenko97@gmail.com>
2024-04-24 14:41:37 +0300

    Rest points api

--- src/main/java/fi/haagahelia/quizzer/controller/QuizAppRestController.java
+++ src/main/java/fi/haagahelia/quizzer/controller/QuizAppRestController.java
@@ -1,15 +1,21 @@
 package fi.haagahelia.quizzer.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import fi.haagahelia.quizzer.repository.CategoryRepository;
+import fi.haagahelia.quizzer.repository.QuizRepository;
 import fi.haagahelia.quizzer.model.Category;
+import fi.haagahelia.quizzer.model.Quiz;
+
 
 @RestController
 @RequestMapping("/api/QuizApp")
@@ -18,12 +24,22 @@ public class QuizAppRestController {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private QuizRepository quizRepository;
 
-	@GetMapping("/categories")
-	public @ResponseBody List<Category> getCategories() {
+    @GetMapping("/categories")
+    public @ResponseBody List<Category> getCategories() {
+        return categoryRepository.findAllByOrderByNameAsc();
+    }
 
-		return categoryRepository.findAll();
-
-	}
-
+    @GetMapping("/quiz/{id}")
+    public @ResponseBody Quiz getQuizById(@PathVariable("id") Long id) {
+        Optional<Quiz> existingQuizOptional = quizRepository.findById(id);
+        if (existingQuizOptional.isPresent()) {
+            Quiz existingQuiz = existingQuizOptional.get();
+            return existingQuiz;
+        }	
+    
+        return null;
+    }
 }
--- src/main/java/fi/haagahelia/quizzer/repository/CategoryRepository.java
+++ src/main/java/fi/haagahelia/quizzer/repository/CategoryRepository.java
@@ -11,4 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByName(String name);
+
+    List<Category> findAllByOrderByNameAsc();
 }
