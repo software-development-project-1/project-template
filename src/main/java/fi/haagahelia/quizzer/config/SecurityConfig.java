f0c816583f8ad4f85183f69e6be381ff2dc417ef
Ariel <bhe099@myy.haaga-helia.fi>
2024-05-08 21:43:22 +0300

    securityconfig permit all direct to QuizzesList endpoint and registrtion success

--- src/main/java/fi/haagahelia/quizzer/config/SecurityConfig.java
+++ src/main/java/fi/haagahelia/quizzer/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import fi.haagahelia.quizzer.service.UserDetailsServiceImpl;
@@ -19,6 +20,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Autowired
 	private UserDetailsServiceImpl userDetailService;
@@ -28,6 +33,8 @@ public class SecurityConfig {
         http
         .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(antMatcher("/css/**")).permitAll()
+                .requestMatchers(antMatcher("/css/**")).permitAll()
+                .requestMatchers(antMatcher("/api/**")).permitAll()
                 .requestMatchers(antMatcher("/h2-console/**")).permitAll()
                 .requestMatchers(antMatcher("/registration/**")).permitAll()
                 .requestMatchers(antMatcher("/saveuser/**")).permitAll()
